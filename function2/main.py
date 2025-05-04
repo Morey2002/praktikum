f7ce7746eb5215b422d939a4c9d65fb2647f3870
Polina Buchina <PolinaBuchina2002@yandex.ru>
2025-05-04 22:58:23 +0300

    empty code

--- function2/main.py
+++ function2/main.py
@@ -21,3 +21,4 @@ def cakes(recipe, available):
 print(cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}))
 print(cakes({"apples": 3, "flour": 300, "sugar": 100, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000}))
 print(task(5))
+print(task(4))
