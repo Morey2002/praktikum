2a9fc878a89aee902771671fff425d21ca7e7126
Polina Buchina <PolinaBuchina2002@yandex.ru>
2025-05-14 21:35:17 +0300

    new header

--- function2/main.py
+++ function2/main.py
@@ -4,5 +4,12 @@ import pprint
 response = requests.get('https://api.github.com')
 response_json = response.json()
 #print(response.status_code)
-pprint.pprint(response_json)
-print(type(response_json))
+#pprint.pprint(response_json)
+#print(type(response_json))
+print(response.headers)
+print(response.request.headers)
+
+user_agent = {'User-Agent': 'I am here again'}
+response = requests.get('https://api.github.com', headers=user_agent)
+
+print(response.request.headers)
