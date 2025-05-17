872a74b20584f5a2f2294abf45b2d9d05bbb1cb2
Polina Buchina <PolinaBuchina2002@yandex.ru>
2025-05-17 22:47:14 +0300

    Generator password

--- function2/config.py
+++ function2/config.py
@@ -1,4 +1,64 @@
 host = "127.0.0.1"
 user = "postgres"
-password = "z"
+password = "19741980"
 db_name = "Saby"
+
+from config import host, user, password, db_name
+import psycopg2
+from psycopg2 import Error
+
+connection = None
+cursor = None
+
+try:
+    connection = psycopg2.connect(
+        host="127.0.0.1",
+        user="postgres",
+        password="19741980",
+        database="postgres",
+        port='5555'
+    )
+    connection.autocommit = True
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+
+        print(f"Serever Version: {cursor.fetchone()}")
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """CREATE TABLE users(
+                id serial PRIMARY KEY,
+                first_name varchar(50) NOT NULL,
+                nick_name varchar(50) NOT NULL)"""
+        )
+
+        print()
+        print("[INFO] Table created successfully")
+
+
+except Error as error:
+    print("Ошибка подключения:", error)
+
+finally:
+    if cursor is not None:
+        cursor.close()
+    if connection is not None and not connection.closed:
+        connection.close()
+        print("Соединение закрыто")
+
+'''response = requests.get('https://api.github.com')
+response_json = response.json()
+print(response.status_code)
+pprint.pprint(response_json)
+print(type(response_json))
+print(response.headers)
+print(response.request.headers)
+
+user_agent = {'User-Agent': 'I am here again'}
+response = requests.get('https://api.github.com', headers=user_agent)
+
+print(response.request.headers)'''
+
--- function2/main.py
+++ function2/main.py
@@ -1,54 +1,6 @@
-from config import host, user, password, db_name
-import requests
-import pprint
-import psycopg2
-from psycopg2 import Error
-
-connection = None
-cursor = None
-
-try:
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name,
-        port='5433'
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT version();")
-    print(cursor.fetchone())
-
-except Error as error:
-    print("Ошибка подключения:", error)
-
-finally:
-    if cursor is not None:
-        cursor.close()
-    if connection is not None and not connection.closed:
-        connection.close()
-        print("Соединение закрыто")
-
-
-
-
-
-
-
-
-
-
-
-'''response = requests.get('https://api.github.com')
-response_json = response.json()
-print(response.status_code)
-pprint.pprint(response_json)
-print(type(response_json))
-print(response.headers)
-print(response.request.headers)
-
-user_agent = {'User-Agent': 'I am here again'}
-response = requests.get('https://api.github.com', headers=user_agent)
-
-print(response.request.headers)'''
-
+import random
+import string
+total = string.ascii_letters + string.digits + string.punctuation
+length = 16
+password = "".join(random.sample(total, length))
+print(password)
